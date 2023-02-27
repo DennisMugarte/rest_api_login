0d97b11bc3f8c3f6bd0bb672f6f1b1ceb6be19e7
Brandon-Cih <107282406+Brandon-Cih@users.noreply.github.com>
2023-02-27 11:46:56 -0600

    index

--- src/index.js
+++ src/index.js
@@ -9,6 +9,15 @@ app.set('json spaces', 2);
 
 app.use(index_routes)
 app.use(pastel_routes)
+//si no funciona elimina lo de abajo
+app.use('/api', pastelesRoutes)
+//arriba xd
+
+app.use((req, res, next) =>{
+    res.status(404).json({
+       error: 'surgio un error'
+    })
+})
 
 app.listen(PORT)
 console.log('Server running on port 3000')
