e40ffcc8440ca2a26ec23e2bb981c03889e8b42c
Brandon-Cih <107282406+Brandon-Cih@users.noreply.github.com>
2023-02-27 11:42:47 -0600

    config/index

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import express from "express"
 import pastel_routes from './routes/pastel.routes.js'
 import index_routes from './routes/index.routes.js'
-
+import {PORT} from "./config.js";
 const app = express()
 
 app.use(express.json())
@@ -10,5 +10,6 @@ app.set('json spaces', 2);
 app.use(index_routes)
 app.use(pastel_routes)
 
-app.listen(3000)
+app.listen(PORT)
 console.log('Server running on port 3000')
+
