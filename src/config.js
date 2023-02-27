2431d9a08e89e2139547fa58368b5f31efb285d4
Brandon-Cih <107282406+Brandon-Cih@users.noreply.github.com>
2023-02-27 11:44:09 -0600

    ss

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ import { config } from "dotenv";
 config()
 
 export const PORT = process.env.PORT || 3000
-const DB_USER =process.env.DB_USER | 'root'
+const DB_USER =process.env.DB_USER || 'root'
 const DB_PASSWORD = process.env.DBPASSWORD || 'holaxd'
 const DB_HOST = process.env.DB_HOST || 'localhost'
 const DB_DATABASE = process.env.DB_DATABASE || 'compnydb'
