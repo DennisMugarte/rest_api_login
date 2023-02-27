0f1e4ba03bd3bc2d987c626858d0768330366442
Dennis_Mugarte <107282802+DennisMugarte@users.noreply.github.com>
2023-02-27 15:45:25 -0600

    Nuevo archivo app.js

--- src/app.js
+++ src/app.js
@@ -2,22 +2,21 @@ import express from "express"
 import pastel_routes from './routes/pastel.routes.js'
 import index_routes from './routes/index.routes.js'
 
-
 const app = express()
 
 app.use(express.json())
-
 app.set('json spaces', 2);
 
 app.use(index_routes)
-app.use(pastel_routes)
+// app.use(pastel_routes)
 //si no funciona elimina lo de abajo
-app.use('/api', pastelesRoutes)
+app.use('/api/pasteria&daniel&el&travieso', pastel_routes)
 //arriba xd
 
+//* Para tirar un error si la URL no es la correcta
 app.use((req, res, next) =>{
     res.status(404).json({
-       error: 'surgio un error'
+        Error: 'Surgio un error'
     })
 })
 
--- src/index.js
+++ src/index.js
@@ -1,30 +1,5 @@
-import express from "express"
-import pastel_routes from './routes/pastel.routes.js'
-import index_routes from './routes/index.routes.js'
+import app from './app.js'
 import {PORT} from './config.js'
-
-const app = express()
-
-app.use(express.json())
-app.set('json spaces', 2);
-
-app.use(index_routes)
-// app.use(pastel_routes)
-//si no funciona elimina lo de abajo
-app.use('/api/pasteria&daniel&el&travieso', pastel_routes)
-//arriba xd
-
-
-//* Para tirar un error si la URL no es la correcta
-app.use((req, res, next) =>{
-    res.status(404).json({
-        Error: 'Surgio un error'
-    })
-})
-
-import app from "./app"
-import { PORT } from "./config.js"
-
 
 app.listen(PORT)
 console.log('Server running on port', PORT)
